4352cd6777fd24e549e61b776b7e7dd472ef48f2
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2022-03-31 11:24:01 -0500

    Update miRNA-Seq.py

--- mi-mRNA-Seq_PRJNA545590/miRNA-Seq.py
+++ mi-mRNA-Seq_PRJNA545590/miRNA-Seq.py
@@ -25,22 +25,22 @@ rule miRNA_Quant:
         "{wd}/{sample}/miRNAs_expressed_all_samples_{sample}.csv"
     run:
         #Make variable for raid and pathway to corresponding directory.
-        raid=str({output}).split("/")[1]
-        path=DIR+"/"+raid + "/"
+        raid=str({output}).split("/")[1]  
+        path=DIR+"/"+raid + "/"      
         #Download fastq(s) from SRA. Run through trim_galore
-        sra.SRA(raid,directory=DIR).trim(trim_galore)
-        #Processes reads for quantification
+        sra.SRA(raid,directory=DIR).trim(trim_galore)               
+        #Processes reads for quantification 
         shell("mapper.pl "+ path + raid + "_1_trimgalore.fastq  -e -m -h \
             -g " + raid[-3:] + " \
             -s " + path + raid + ".fasta")
-        #Quantification
+        #Quantification 
         shell("quantifier.pl \
             -p hairpin.fa \
             -m mature.fa \
             -r " + path + raid + ".fasta \
             -y " + raid + " \
             -t rno")
-
+            
         #Move output
         shell("mv *" + raid + "* " + path)
 
@@ -49,20 +49,20 @@ rule Count_Table:
         ["{wd}/{sample}/miRNAs_expressed_all_samples_{sample}.csv".format(wd=DIR,sample=s) for s in ra]
     output:
         "countTable.tsv"
-    run:
+    run:           
         x=0
         for qf in input:
-            raid=qf.split('/')[1]
-            path=DIR+"/"+raid + "/"
-            #Read in miRNAs_expressed_all_samples_{sample}.csv
-            quant_df=pd.read_csv(path+"miRNAs_expressed_all_samples_"+raid+".csv",sep="\t")
+            raid=qf.split('/')[1] 
+            path=DIR+"/"+raid + "/"        
+            #Read in miRNAs_expressed_all_samples_{sample}.csv 
+            quant_df=pd.read_csv(path+"miRNAs_expressed_all_samples_"+raid+".csv",sep="\t")  
             # Get #miRNA  and precursor IDs from miRNAs_expressed_all_samples_{sample}.csv
-            if x==0:
+            if x==0:                
                 countTable=quant_df[["#miRNA","precursor"]]
-                countTable = countTable.rename(columns={'#miRNA': 'miRNA'})
+                countTable = countTable.rename(columns={'#miRNA': 'miRNA'})                                                     
                 x=1
-             # Extract counts then add to table.
+             # Extract counts then add to table.               
             countTable[raid]=quant_df[raid[-3:]]
-        #Mature miRNA's can have more than 1 precursor. For this type of mature miRNA, this takes the average across precursors.
-        countTable = countTable.groupby(['miRNA'],as_index = False).mean()
+        #Mature miRNA's can have more than 1 precursor. For this type of mature miRNA, this takes the average across precursors. 
+        countTable = countTable.groupby(['miRNA'],as_index = False).mean()  
         countTable.to_csv("countTable.tsv",mode="w", header=True,index=False,sep="\t")
