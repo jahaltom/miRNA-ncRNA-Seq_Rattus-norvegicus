ff93f84f8b30a2c78876375a95bc014a75e9da62
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2022-03-31 11:03:22 -0500

    Create Prepare_data.sh

---
+++ mi-mRNA-Seq_PRJNA545590/Prepare_data.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+
+mkdir Rat_data
+cd Rat_data
+
+#download Rat ncrna transcriptome 
+wget http://ftp.ensembl.org/pub/release-105/fasta/rattus_norvegicus/ncrna/Rattus_norvegicus.mRatBN7.2.ncrna.fa.gz
+
+
+#download Rat Genome
+wget http://ftp.ensembl.org/pub/release-105/fasta/rattus_norvegicus/dna/Rattus_norvegicus.mRatBN7.2.dna.toplevel.fa.gz
+
+#unzip all
+gunzip *.gz
+
+
+
+#create decoy list
+grep ">" Rattus_norvegicus.mRatBN7.2.dna.toplevel.fa | cut -d " " -f 1 | tr -d ">" > decoys.txt
+        
+
+#combine transcriptomes and decoy fasta files.   
+cat  Rattus_norvegicus.mRatBN7.2.ncrna.fa Rattus_norvegicus.mRatBN7.2.dna.toplevel.fa > RatTranscrptome_decoy.fasta
+
+
+#cleanup
+rm Rattus_norvegicus.mRatBN7.2.ncrna.fa Rattus_norvegicus.mRatBN7.2.dna.toplevel.fa
+
+
+#create salmon index
+time salmon index -t RatTranscrptome_decoy.fasta -d decoys.txt -p 15 -i salmon_index
+
