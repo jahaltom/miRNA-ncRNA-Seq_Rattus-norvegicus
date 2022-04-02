d1b46aa562e9b2ce705b9a2d5c6d6932b00187d2
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2022-04-02 09:37:02 -0500

    Update DESeq2_RatHumanOrthologMetadata.r

--- mi-mRNA-Seq_PRJNA545590/DESeq2_RatHumanOrthologMetadata.r
+++ mi-mRNA-Seq_PRJNA545590/DESeq2_RatHumanOrthologMetadata.r
@@ -5,7 +5,7 @@ library(dplyr)
 
 
 #Read in count information.
-countData = read.table("out/results_Count_gene.tsv",header=TRUE,sep = '\t',row.names="ensembl_gene_id_version_Rat")
+countData = read.table("out/results_Count_gene.tsv",header=TRUE,sep = '\t',row.names="Rat_ensembl_gene_id_version")
 
 
 
@@ -28,7 +28,7 @@ result = results(dds, contrast=c("Sample","case","control"))
 ## Remove rows with NA
 result = result[complete.cases(result),]
 #Put GeneID as column
-result = cbind(ensembl_gene_id_version_Rat = rownames(result), result)
+result = cbind(Rat_ensembl_gene_id_version = rownames(result), result)
 
 
 
@@ -42,15 +42,16 @@ rat = useMart("ensembl", dataset = "rnorvegicus_gene_ensembl")
 
 #Read in ensembl_transcript_id_version IDs
 ID_List = read.table("out/results_TPM_tx.tsv",header=TRUE)
-ID_List=as.vector(unlist(ID_List['ensembl_transcript_id_version']))
+ID_List=as.vector(unlist(ID_List['Rat_ensembl_transcript_id_version']))
+
 
 #Gather Rat data
 df_Rat=getBM(attributes = c("ensembl_transcript_id_version","ensembl_gene_id_version","ensembl_gene_id","gene_biotype","description"), 
                  filters = "ensembl_transcript_id_version", 
                  values = ID_List , 
                  mart = rat)
-#Add Rat Sufix to all columns
-colnames(df_Rat) <- paste(colnames(df_Rat),"Rat",sep="_")   
+#Add Rat Prefix to all columns
+colnames(df_Rat) <- paste("Rat",colnames(df_Rat),sep="_")   
 
               
 #Gather RatHuman Ortholog data                 
@@ -62,27 +63,27 @@ df_RatHumanOrtho = getLDS(attributes = c("ensembl_transcript_id_version","hsapie
                  martL = human, 
                  uniqueRows=T)
 
-#Add Human Sufix to all columns
-colnames(df_RatHumanOrtho) <- paste(colnames(df_RatHumanOrtho),"Human",sep="_")
+#Add Human Prefix to all columns
+colnames(df_RatHumanOrtho) <- paste("Human",colnames(df_RatHumanOrtho),sep="_")   
 #Rename so that dfs have matching ensembl_transcript_id_version column.                   
-names(df_RatHumanOrtho)[names(df_RatHumanOrtho) == "Transcript.stable.ID.version_Human"] <- "ensembl_transcript_id_version_Rat"    
-names(df_RatHumanOrtho)[names(df_RatHumanOrtho) == "Human.homology.type_Human"] <- "Human.homology.type"            
+names(df_RatHumanOrtho)[names(df_RatHumanOrtho) == "Human_Transcript.stable.ID.version"] <- "Rat_ensembl_transcript_id_version"    
+names(df_RatHumanOrtho)[names(df_RatHumanOrtho) == "Human_Human.homology.type"] <- "Rat_Human.homology.type"            
 
 #Merge dfs by ensembl_transcript_id_version
-RatHumanOrthoMerged= merge(df_RatHumanOrtho,df_Rat,by="ensembl_transcript_id_version_Rat",all=TRUE)
+RatHumanOrthoMerged= merge(df_RatHumanOrtho,df_Rat,by="Rat_ensembl_transcript_id_version",all=TRUE)
 
 
 
 ##Drop transcript columns, unique rows, and merge.
-RatHumanOrthoMerged=RatHumanOrthoMerged %>% select(-contains("ensembl_transcript_id_version_Rat"))
+RatHumanOrthoMerged=RatHumanOrthoMerged %>% select(-contains("Rat_ensembl_transcript_id_version"))
 RatHumanOrthoMerged=distinct(RatHumanOrthoMerged)
 
 #Merge with DESeq2 
-RatHumanOrthoDGE = merge(RatHumanOrthoMerged,result,by="ensembl_gene_id_version_Rat",all.y=TRUE)
+RatHumanOrthoDGE = merge(RatHumanOrthoMerged,result,by="Rat_ensembl_gene_id_version",all.y=TRUE)
 write.table(RatHumanOrthoDGE,"RatHumanOrthoDGE.tsv" ,sep = '\t',row.names = FALSE)
 
 #Merge with counts. 
 #Put GeneID as column
-countData = cbind(ensembl_gene_id_version = rownames(countData), countData)
-RatHumanOrthoCounts = merge(RatHumanOrthoMerged,countData,by="ensembl_gene_id_version_Rat",all=TRUE)
+countData = cbind(Rat_ensembl_gene_id_version = rownames(countData), countData)
+RatHumanOrthoCounts = merge(RatHumanOrthoMerged,countData,by="Rat_ensembl_gene_id_version",all=TRUE)
 write.table(RatHumanOrthoCounts,"RatHumanOrthoCounts.tsv" ,sep = '\t',row.names = FALSE)
