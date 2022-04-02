943fbad4f32ab18c57f44c13478348cf5b8b2008
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2022-04-01 23:10:56 -0500

    Update DESeq2_RatHumanOrthologMetadata.r

--- mi-mRNA-Seq_PRJNA545590/DESeq2_RatHumanOrthologMetadata.r
+++ mi-mRNA-Seq_PRJNA545590/DESeq2_RatHumanOrthologMetadata.r
@@ -5,7 +5,7 @@ library(dplyr)
 
 
 #Read in count information.
-countData = read.table("out/results_Count_gene.tsv",header=TRUE,sep = '\t',row.names="ensembl_gene_id_version")
+countData = read.table("out/results_Count_gene.tsv",header=TRUE,sep = '\t',row.names="ensembl_gene_id_version_Rat")
 
 
 
@@ -28,7 +28,7 @@ result = results(dds, contrast=c("Sample","case","control"))
 ## Remove rows with NA
 result = result[complete.cases(result),]
 #Put GeneID as column
-result = cbind(ensembl_gene_id_version = rownames(result), result)
+result = cbind(ensembl_gene_id_version_Rat = rownames(result), result)
 
 
 
@@ -49,7 +49,10 @@ df_Rat=getBM(attributes = c("ensembl_transcript_id_version","ensembl_gene_id_ver
                  filters = "ensembl_transcript_id_version", 
                  values = ID_List , 
                  mart = rat)
-                 
+#Add Rat Sufix to all columns
+colnames(df_Rat) <- paste(colnames(df_Rat),"Rat",sep="_")   
+
+              
 #Gather RatHuman Ortholog data                 
 df_RatHumanOrtho = getLDS(attributes = c("ensembl_transcript_id_version","hsapiens_homolog_orthology_type"), 
                  filters = "ensembl_transcript_id_version", 
@@ -59,24 +62,27 @@ df_RatHumanOrtho = getLDS(attributes = c("ensembl_transcript_id_version","hsapie
                  martL = human, 
                  uniqueRows=T)
 
-#Rename so that dfs have matching ensembl_transcript_id_version column                   
-names(df_RatHumanOrtho)[names(df_RatHumanOrtho) == "Transcript.stable.ID.version"] <- "ensembl_transcript_id_version"               
+#Add Human Sufix to all columns
+colnames(df_RatHumanOrtho) <- paste(colnames(df_RatHumanOrtho),"Human",sep="_")
+#Rename so that dfs have matching ensembl_transcript_id_version column.                   
+names(df_RatHumanOrtho)[names(df_RatHumanOrtho) == "Transcript.stable.ID.version_Human"] <- "ensembl_transcript_id_version_Rat"    
+names(df_RatHumanOrtho)[names(df_RatHumanOrtho) == "Human.homology.type_Human"] <- "Human.homology.type"            
+
 #Merge dfs by ensembl_transcript_id_version
-RatHumanOrthoMerged= merge(df_RatHumanOrtho,df_Rat,by="ensembl_transcript_id_version",all=TRUE)
+RatHumanOrthoMerged= merge(df_RatHumanOrtho,df_Rat,by="ensembl_transcript_id_version_Rat",all=TRUE)
+
 
 
 ##Drop transcript columns, unique rows, and merge.
-RatHumanOrthoMerged=RatHumanOrthoMerged %>% select(-contains("ensembl_transcript_id_version"))
+RatHumanOrthoMerged=RatHumanOrthoMerged %>% select(-contains("ensembl_transcript_id_version_Rat"))
 RatHumanOrthoMerged=distinct(RatHumanOrthoMerged)
 
 #Merge with DESeq2 
-RatHumanOrthoDGE = merge(RatHumanOrthoMerged,result,by="ensembl_gene_id_version",all.y=TRUE)
+RatHumanOrthoDGE = merge(RatHumanOrthoMerged,result,by="ensembl_gene_id_version_Rat",all.y=TRUE)
 write.table(RatHumanOrthoDGE,"RatHumanOrthoDGE.tsv" ,sep = '\t',row.names = FALSE)
 
 #Merge with counts. 
 #Put GeneID as column
 countData = cbind(ensembl_gene_id_version = rownames(countData), countData)
-RatHumanOrthoCounts = merge(RatHumanOrthoMerged,countData,by="ensembl_gene_id_version",all=TRUE)
+RatHumanOrthoCounts = merge(RatHumanOrthoMerged,countData,by="ensembl_gene_id_version_Rat",all=TRUE)
 write.table(RatHumanOrthoCounts,"RatHumanOrthoCounts.tsv" ,sep = '\t',row.names = FALSE)
-
-
